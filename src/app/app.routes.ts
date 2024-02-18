9fd07ed4c7893b11557477882d99ed71f278fe5e
Damian Vasquez <Pedro.Damian.Vasquez.Calle@dnv.com>
2024-02-18 15:11:10 -0400

    Initial commit + basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { DepositsComponent } from './components/deposits/deposits.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'deposits', component: DepositsComponent },
+];
